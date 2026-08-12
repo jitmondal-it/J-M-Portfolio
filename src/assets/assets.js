66437c2f8f12c82a441ed59a10101e994a9a39d7
jitmondal-it <jitmondal631@gmail.com>
2026-08-12 19:48:24 +0530

    Profile Image

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -10,7 +10,7 @@ import {
 import { FaStripe } from "react-icons/fa6"; // Stripe icon is in fa6
 
 export const assets = {
-  profileImg: "/profileImg.jpeg",
+  profileImg: "/j-pic.jpeg",
 };
 
 export const aboutInfo = [
